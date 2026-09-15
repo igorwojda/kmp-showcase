dbe1ad78246b8402c1f88d7d16cecaf180843724
Igor Wojda <igor.wojda@gmail.com>
2026-09-15 13:51:00 +0200

    Initial commit

---
+++ sharedLogic/src/androidMain/kotlin/com/igorwojda/showcase/Platform.android.kt
@@ -0,0 +1,9 @@
+package com.igorwojda.showcase
+
+import android.os.Build
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
---
+++ sharedLogic/src/commonMain/kotlin/com/igorwojda/showcase/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.igorwojda.showcase
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
