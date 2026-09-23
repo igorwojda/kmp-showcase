c3588052ed6370a457a90f28c3a9d31d98f016e9
Igor Wojda <igor.wojda@gmail.com>
2026-09-23 13:00:59 +0200

    Update Koin.ios.kt to clarify TODO comment regarding instance provision

--- sharedLogic/src/iosMain/kotlin/com/igorwojda/showcase/di/Koin.ios.kt
+++ sharedLogic/src/iosMain/kotlin/com/igorwojda/showcase/di/Koin.ios.kt
@@ -3,6 +3,6 @@ package com.igorwojda.showcase.di
 import com.igorwojda.showcase.presentation.forecast.ForecastViewModel
 import org.koin.mp.KoinPlatform
 
-// TODO: Another way to provide instance?
+// TODO: Another way to provide instance? Perhaps delegate exists?
 /** Swift can't use Koin's reified `get()`, so every resolved type needs an explicit accessor. */
 fun provideForecastViewModel(): ForecastViewModel = KoinPlatform.getKoin().get()
